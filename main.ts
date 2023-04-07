5d3e0ffafb31e8fdf72d4971b9e26c875674f0cf
aihuaqiao <aihuaqiao@shoplineapp.com>
2023-04-07 14:41:30 +0800

    fix: 添加

--- main.ts
+++ main.ts
@@ -1,40 +1,58 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
-const decoder = new TextDecoder("utf-8");
- 
-const listen = Deno.listen({ port: 80 })
-for await (const conn of listen) {
-    for await (const req of Deno.serveHttp(conn)) {
-        let url = new URL(req.request.url);
-        let path = url.pathname === "/" ? "/index.html" : url.pathname;
-        let ext = path.substr(path.indexOf("."), path.length);
-        let data = await Deno.readFile(`.${path}`);
-        let res = null;
-        switch (ext) {
-            case ".html":
-                res = new Response(decoder.decode(data));
-                res.headers.set("content-type", "text/html");
-                break;
-            case ".css":
-                res = new Response(decoder.decode(data));
-                res.headers.set("content-type", "text/css");
-                break;
-            case ".js":
-                res = new Response(decoder.decode(data));
-                res.headers.set("content-type", "application/javascript");
-                break;
-            case ".png":
-                res = new Response(data);
-                res.headers.set("content-type", "image/png");
-                break;
-            case ".ico":
-                res = new Response(data);
-                res.headers.set("content-type", "image/x-icon");
-                break;
-            case ".jpg":
-                res = new Response(data);
-                res.headers.set("content-type", "image/jpeg");
-                break;
-        }
-        req.respondWith(res);
-    }
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+
+const getContentType = (name: string) => {
+  if (name.endsWith('.js')) {
+    return 'text/javascript';
+  }
+  if (name.endsWith('.css')) {
+    return 'text/css';
+  }
+  if (name.endsWith('.html')) {
+    return 'text/html; charset=utf-8';
+  }
+  return 'charset=utf-8';
 }
+
+const fileNameMap = {
+  '/': 'index.html',
+  '/test.js': 'test.js',
+  '/partytown.js': 'partytown.js',
+  '/partytown-atomics.js': 'partytown-atomics.js',
+  '/partytown-media.js': 'partytown-media.js',
+  '/partytown-sandbox-sw.js': 'partytown-sandbox-sw.js',
+  '/partytown-sw.js': 'partytown-sw.js',
+}
+
+async function handleRequest(request: Request): Promise<Response> {
+  const url = new URL(request.url);
+  url.host = 'mslynnhair.com';
+  // url.host = 'performance1.myshopline.com';
+  const fileName = fileNameMap[url.pathname];
+
+  if (fileName) {
+    const file = await Deno.readFile(`./${fileName}`);
+    return new Response(file, {
+      headers: {
+        "content-type": getContentType(fileName)
+      },
+    });
+  }
+
+  const res = await fetch(url.toString(), {
+    headers: request.headers,
+    method: request.method,
+    body: request.body,
+  });
+
+  const headers = new Headers(res.headers);
+  // headers.set('cross-origin-embedder-policy', 'credentialless');
+  // headers.set('cross-origin-opener-policy', 'same-origin');
+  
+  return new Response(res.body, {
+    status: res.status,
+    statusText: res.statusText,
+    headers,
+  });
+}
+
+serve(handleRequest);
