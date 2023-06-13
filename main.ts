763cbf486b5a46714d18abcdad29562021d1f01f
aihuaqiao <aihuaqiao@shoplineapp.com>
2023-06-13 15:46:10 +0800

    fix: host

--- main.ts
+++ main.ts
@@ -29,7 +29,7 @@ async function handleRequest(request: Request): Promise<Response> {
   // url.host = 'performance1.myshopline.com';
   // url.host = 'hekkanigeria.com';
   // url.host = 'anniecloth.com';
-  url.host = 'www.maxinejewelry.com';
+  url.host = 'maxinejewelry.com';
   const fileName = fileNameMap[url.pathname];
 
   if (fileName) {
