871121f88c900014c9676f0215794614ca5ec238
aihuaqiao <aihuaqiao@shoplineapp.com>
2023-06-13 11:40:48 +0800

    fix: 首页

--- main.ts
+++ main.ts
@@ -29,7 +29,7 @@ async function handleRequest(request: Request): Promise<Response> {
   // url.host = 'performance1.myshopline.com';
   // url.host = 'hekkanigeria.com';
   // url.host = 'anniecloth.com';
-  url.host = 'alicehuang-test1.myshopline.com';
+  url.host = 'www.maxinejewelry.com';
   const fileName = fileNameMap[url.pathname];
 
   if (fileName) {
