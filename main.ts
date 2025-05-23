be93e5e02d97ed5ada330a76ff027e67ea2bc9d8
mythxiaoai <mythxiaoai@gmail.com>
2025-05-23 16:58:21 +0800

    feat: 头部预压缩

--- main.ts
+++ main.ts
@@ -14,21 +14,11 @@ const getContentType = (name: string) => {
 }
 
 const fileNameMap = {
-  '/': 'index.html',
-  '/test.js': 'test.js',
-  '/partytown.js': 'partytown.js',
-  '/partytown-atomics.js': 'partytown-atomics.js',
-  '/partytown-media.js': 'partytown-media.js',
-  '/partytown-sandbox-sw.js': 'partytown-sandbox-sw.js',
-  '/partytown-sw.js': 'partytown-sw.js',
+  '/': 'index.html'
 }
 
 async function handleRequest(request: Request): Promise<Response> {
   const url = new URL(request.url);
-  // url.host = 'mslynnhair.com';
-  // url.host = 'performance1.myshopline.com';
-  // url.host = 'hekkanigeria.com';
-  // url.host = 'anniecloth.com';
   url.host = 'www.bitget.com';
   const fileName = fileNameMap[url.pathname];
 
@@ -36,7 +26,18 @@ async function handleRequest(request: Request): Promise<Response> {
     const file = await Deno.readFile(`./${fileName}`);
     return new Response(file, {
       headers: {
-        "content-type": getContentType(fileName)
+        "content-type": getContentType(fileName),
+        // "Link": `<https://cdn.myshopline.com>; rel="preconnect", <https://cdn-theme.myshopline.com>; rel="preconnect", <https://img.myshopline.com>; rel="preconnect", <https://img-va.myshopline.com>; rel="preconnect", <https://cdn-theme.myshopline.com/cdn/shop/prod//1739533831686/67d2968cf3641f2d25531bfd/1746799962559/assets/component-tool-tip.css>; rel="preload"; as="style", <https://cdn-theme.myshopline.com/cdn/shop/prod//1739533831686/67d2968cf3641f2d25531bfd/1746799969968/assets/component-product-modal.css>; rel="preload"; as="style", <https://cdn-theme.myshopline.com/cdn/shop/prod//1739533831686/67d2968cf3641f2d25531bfd/1746799969032/assets/section-collapsible-content.css>; rel="preload"; as="style", <https://cdn-theme.myshopline.com/cdn/shop/prod//1739533831686/67d2968cf3641f2d25531bfd/1746799962602/assets/section-main-product.css>; rel="preload"; as="style", <https://cdn-theme.myshopline.com/cdn/shop/prod//1739533831686/67d2968cf3641f2d25531bfd/1746799970625/assets/component-accordion.css>; rel="preload"; as="style", <https://cdn-theme.myshopline.com/cdn/shop/prod//1739533831686/67d2968cf3641f2d25531bfd/1746799962605/assets/section-main-product-media-gallery.css>; rel="preload"; as="style"`
+        "Link": `
+          <https://img.bgstatic.com>; rel="preconnect",
+          </portalx-static/client/css/6f3609aa633de3d72c56.css>; as="style"; rel="preload", 
+          </portalx-static/client/css/f200c74013e4ba0700bf.css>; as="style"; rel="preload",
+          </portalx-static/client/css/a6c0f1b486b804cd1da2.css>; as="style"; rel="preload",
+          </baseasset/fonts/Switzer-Regular.otf>; as="font"; rel="preload"; crossorigin="anonymous",
+          </baseasset/fonts/Switzer-Medium.otf>; as="font"; rel="preload"; crossorigin="anonymous",
+          </baseasset/fonts/Switzer-Bold.otf>; as="font"; rel="preload"; crossorigin="anonymous",
+          </baseasset/fonts/Switzer-Semibold.otf>; as="font"; rel="preload"; crossorigin="anonymous",
+        `.split(',').map(str => str.trim()).join(', ')
       },
     });
   }
@@ -48,8 +49,7 @@ async function handleRequest(request: Request): Promise<Response> {
   });
 
   const headers = new Headers(res.headers);
-  // headers.set('cross-origin-embedder-policy', 'credentialless');
-  // headers.set('cross-origin-opener-policy', 'same-origin');
+  headers.set('cache-control', 'public, max-age=31556952, immutable');
   
   return new Response(res.body, {
     status: res.status,
